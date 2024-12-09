de38d4fc46cfae11007826bae7ed9368b6815612
akshaypozath <71353727+akshaypozath@users.noreply.github.com>
2024-12-09 16:47:36 -0500

    Add files via upload

---
+++ AKSHAY POZATH NYC CRIME DATA PROJECT/cleaningdata.sql
@@ -0,0 +1,28 @@
+SELECT * FROM `Crime DB`.crimedataprecinct2;
+SELECT DISTINCT *
+FROM `Crime DB`.crimedataprecinct2;
+UPDATE `Crime DB`.crimedataprecinct2
+SET `2000` = COALESCE(`2000`, 0), 
+    `2001` = COALESCE(`2001`, 0),
+	`2002` = COALESCE(`2002`, 0),
+    `2003` = COALESCE(`2003`, 0),
+    `2004` = COALESCE(`2004`, 0),
+    `2005` = COALESCE(`2005`, 0),
+    `2006` = COALESCE(`2006`, 0),
+    `2007` = COALESCE(`2007`, 0),
+    `2008` = COALESCE(`2008`, 0),
+    `2009` = COALESCE(`2009`, 0),
+    `2010` = COALESCE(`2010`, 0),
+    `2011` = COALESCE(`2011`, 0),
+    `2012` = COALESCE(`2012`, 0),
+    `2013` = COALESCE(`2013`, 0),
+    `2014` = COALESCE(`2014`, 0),
+    `2015` = COALESCE(`2015`, 0),
+    `2016` = COALESCE(`2016`, 0),
+    `2017` = COALESCE(`2017`, 0),
+    `2018` = COALESCE(`2018`, 0),
+    `2019` = COALESCE(`2019`, 0),
+    `2020` = COALESCE(`2020`, 0),
+    `2021` = COALESCE(`2021`, 0),
+	`2022` = COALESCE(`2022`, 0),
+	`2023` = COALESCE(`2023`, 0);
